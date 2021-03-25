64f43053ab79e1e45bad2bab4e03f33133fe0425
JIaliang Han <hanjiali@umich.edu>
2021-03-26 00:07:34 +0800

    Update Visibility Map.py

--- Visibility Map.py
+++ Visibility Map.py
@@ -1,9 +1,25 @@
-from geometry import Node,Edge,Vertice,Polygon
+from geometry import Node,Edge,Vertice,Polygon,intersect
+from math import atan2,pi
+import bisect
+
 def get_vertex_list(node,vertices):
     vertex_list={}
     for vertex in vertices:
-        vertex_list
-    return vertex_list
+        delta_x=vertex.node.x-node.x
+        delta_y=vertex.node.y-node.y
+        angle=atan2(delta_y,delta_x)
+        if angle<0:
+            angle=angle+2*pi
+        vertex_list[vertex]=angle
+        vertex_list1 = sorted(vertex_list.items(), key=lambda x: x[1], reverse=False)
+    return vertex_list1
+
+def visible(node1,node2,obstacle_list):
+    line=Edge(node1,node2)
+    for edge in obstacle_list.edge:
+        if intersect(line,edge):
+            return False
+    return True
 
 class Visibility_Map:
     def __init__(self,start,goal, obstacle):
@@ -23,8 +39,10 @@ class Visibility_Map:
         # sorted edges list that intersect horizontal half-line emanating from node
         active_list=self.get_active_list(node,vertices)
         for angle in vertex_list:
-            if self.visible(node,angle.node):
+            if self.visible(node,angle.node,self.obstacles):
                 self.visibility_graph.append(Edge(node,angle,node))
+            if angle[0].edge_position=="start" and angle[0].edge not in active_list:
+                active_list.append(angle[0].edge)
 
 
 
